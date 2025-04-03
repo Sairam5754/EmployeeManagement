5f489694b0afbc6e7e21d765139974f378019a9b
kotta <kotta@LAPTOP-EUDAPNLS>
2025-04-03 21:03:51 +0530

    Add project files.

---
+++ EmployeeManagement/Controllers/Models/EmployeeRegistrationDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeManagement.Controllers.Models
+{
+	public class EmployeeRegistrationDTO
+	{
+		public string Name { get; set; }
+		public string Designation { get; set; }
+		public DateTime DateOfJoin { get; set; }
+		public decimal Salary { get; set; }
+		public string Gender { get; set; }
+		public string State { get; set; }
+	}
+}
