25f4c7305582ee9c4b1a2e28e9de960a5d89d987
kothapally sairam <112247788+Sairam5754@users.noreply.github.com>
2025-04-05 20:54:53 +0530

    Update Program.cs

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -4,14 +4,26 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<MyDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowFrontend",
+		policy => policy.WithOrigins("http://localhost:3000") // 👈 React frontend URL
+						.AllowAnyHeader()
+						.AllowAnyMethod());
+});
+
+
 
 var app = builder.Build();
 
@@ -24,8 +36,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors("AllowFrontend");
 
 app.Run();
